fd16ad162a671af99324ac136f7981f05d6783e1
Kevin Yeh <kevinyeh@Kevins-MBP-3.attlocal.net>
2018-07-15 22:49:10 -0700

    redesigning 3 step process;chnaged logo header;added what we do section;

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,6 +3,7 @@ var button = $('<button>');
 var span = $('<span>').html('Start Design');
 newBut.html(button);
 button.html(span);
+button.addClass('startbutton');
 
 
 $('#sec1But').html(newBut);
@@ -13,17 +14,35 @@ $(document).on('scroll', function() {
     span = $('<span>').html('Start Design');
     newBut.html(button);
     button.html(span);
+    button.addClass('startbutton');
     if ($(document).scrollTop()) {
         $('header').addClass('color');
         $('#headerBut').html(newBut);
         $('#sec1But').empty();
+        $('#logo').attr('src', 'assets/svg/tgjlogoheader2.svg');
     } else {
         $('header').removeClass('color');
         $('#headerBut').empty();
         $('#sec1But').html(newBut);
+        $('#logo').attr('src', 'assets/svg/tgjlogoheader(white)2.svg');
     }
 });
 
 $(document).on('click', '.top', function() {
     $('html, body').animate({ scrollTop: 0 }, 1000);
 });
+
+$(document).on('click', '#steponebutton', function() {
+	$('#steptext > p').removeClass('active');
+	$('#stepone').addClass('active');
+});
+
+$(document).on('click', '#steptwobutton', function() {
+	$('#steptext > p').removeClass('active');
+	$('#steptwo').addClass('active');
+})
+
+$(document).on('click', '#stepthreebutton', function() {
+	$('#steptext > p').removeClass('active');
+	$('#stepthree').addClass('active');
+})
