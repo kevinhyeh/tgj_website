f5d228180af9f1a557053c1c487cd361040f2938
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro-3.local>
2018-07-27 15:02:58 -0700

    added styling to 3 step process

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,7 +4,7 @@ var span = $('<span>').html('Start Design');
 newBut.html(button);
 button.html(span);
 button.addClass('startbutton');
- 
+
 
 $('#sec1But').html(newBut);
 
@@ -28,27 +28,49 @@ $(document).on('scroll', function() {
         $('#logo').attr('src', 'assets/svg/tgjlogoheader(white)2.svg');
     }
 });
+// process slideshow
+    var steps = 0;
 
-$('#steponebutton').on('click', function() {
-    $('.process').removeClass('processactive');
-    $(this).addClass('processactive');
-    $('#steptwoprocess, #stepthreeprocess').addClass('active');
-    $('#steponeprocess').removeClass('active');
-});
+    function differentSteps() {
+        if (steps == 0) {
+            $('#stepstitle').html("1. Choose Your Project");
+            $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
+            $('#boxone').addClass('activestep');
+            $('#vbcontent').html("Put step one picture example here");
+        }
+        if (steps == 1) {
+            $('#stepstitle').empty();
+            $('#stepstitle').html("2. Take Our Survey");
+            $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
+            $('#boxtwo').addClass('activestep');
+            $('#vbcontent').html("Put step two picture example here");
+        }
+        if (steps == 2) {
+            $('#stepstitle').html("3. Submit And Wait");
+            $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
+            $('#boxthree').addClass('activestep');
+            $('#vbcontent').html("Put step three picture example here");
+        }
+    }
 
-$('#steptwobutton').on('click', function() {
-    $('.process').removeClass('processactive');
-    $(this).addClass('processactive');
-    $('#steponeprocess, #stepthreeprocess').addClass('active');
-    $('#steptwoprocess').removeClass('active');
-});
+    differentSteps();
 
-$('#stepthreebutton').on('click', function() {
-    $('.process').removeClass('processactive');
-    $(this).addClass('processactive');
-    $('#steponeprocess, #steptwoprocess').addClass('active');
-    $('#stepthreeprocess').removeClass('active');
-});
+    $('#prev, #next').on('click', function() {
+        $('#diffsteps .circles').removeClass('activestep');
+        if ($(this).attr("id") == "prev") {
+            steps--;
+            if (steps == -1) {
+                steps = 2;
+            }
+        }
+        if ($(this).attr("id") == "next") {
+            steps++;
+            if (steps == 3) {
+                steps = 0;
+            }
+        }
+        differentSteps();
+    });
 
 $('#chooseproject').hide();
 // form page start button
