916fbadd309951abb17d2c0c7e366a5aef6ada43
Kevin Yeh <kevinyeh@Kevins-MBP-3.attlocal.net>
2018-07-29 17:28:50 -0700

    seperated css files; added whywediff pics; added contentone and contenttwo to sec3;

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -28,49 +28,63 @@ $(document).on('scroll', function() {
         $('#logo').attr('src', 'assets/svg/tgjlogoheader(white)2.svg');
     }
 });
+
 // process slideshow
-    var steps = 0;
+    var steps = 1;
 
     function differentSteps() {
-        if (steps == 0) {
+        if (steps == 1) {
             $('#stepstitle').html("1. Choose Your Project");
             $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
             $('#boxone').addClass('activestep');
-            $('#vbcontent').html("Put step one picture example here");
+            $('#contentone').removeClass('showprocess').fadeIn();
+            $('#contentone div').removeClass('showprocess');
         }
-        if (steps == 1) {
+        if (steps == 2) {
             $('#stepstitle').empty();
             $('#stepstitle').html("2. Take Our Survey");
             $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
             $('#boxtwo').addClass('activestep');
-            $('#vbcontent').html("Put step two picture example here");
+            $('#contenttwo').removeClass('showprocess');
+            $('#contenttwo div').removeClass('showprocess');
         }
-        if (steps == 2) {
+        if (steps == 3) {
             $('#stepstitle').html("3. Submit And Wait");
             $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
             $('#boxthree').addClass('activestep');
-            $('#vbcontent').html("Put step three picture example here");
+            $('#contentthree').removeClass('showprocess');
         }
     }
 
     differentSteps();
 
-    $('#prev, #next').on('click', function() {
+    $('#prev, #next, #boxone, #boxtwo, #boxthree, #rightarrow, #leftarrow').on('click', function() {
+        $('#vbcontent div').addClass('showprocess');
         $('#diffsteps .circles').removeClass('activestep');
-        if ($(this).attr("id") == "prev") {
+        if ($(this).attr("id") == "prev" || $(this).attr("id") == "leftarrow") {
             steps--;
-            if (steps == -1) {
-                steps = 2;
+            if (steps == 0) {
+                steps = 3;
             }
         }
-        if ($(this).attr("id") == "next") {
+        if ($(this).attr("id") == "next" || $(this).attr("id") == "rightarrow") {
             steps++;
-            if (steps == 3) {
-                steps = 0;
+            if (steps == 4) {
+                steps = 1;
             }
+        }
+        if ($(this).attr("id") == "boxone") {
+            steps = 1;
+        }
+        if ($(this).attr("id") == "boxtwo") {
+            steps = 2;
+        }
+        if ($(this).attr("id") == "boxthree") {
+            steps = 3;
         }
         differentSteps();
     });
+
 
 $('#chooseproject').hide();
 // form page start button
