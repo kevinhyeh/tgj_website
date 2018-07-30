69b2681c292120b5650658ec9d552175a6004b04
Kevin Yeh <kevinyeh@Kevins-MBP-3.attlocal.net>
2018-07-30 00:48:11 -0700

    added wording to sec2, sec3, sec4, and sec 5; style faq dropdowns; style sec5; styled sec3;

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,7 +5,6 @@ newBut.html(button);
 button.html(span);
 button.addClass('startbutton');
 
-
 $('#sec1But').html(newBut);
 
 // header scroll function
@@ -30,60 +29,69 @@ $(document).on('scroll', function() {
 });
 
 // process slideshow
-    var steps = 1;
+var steps = 1;
 
-    function differentSteps() {
-        if (steps == 1) {
-            $('#stepstitle').html("1. Choose Your Project");
-            $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
-            $('#boxone').addClass('activestep');
-            $('#contentone').removeClass('showprocess').fadeIn();
-            $('#contentone div').removeClass('showprocess');
-        }
-        if (steps == 2) {
-            $('#stepstitle').empty();
-            $('#stepstitle').html("2. Take Our Survey");
-            $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
-            $('#boxtwo').addClass('activestep');
-            $('#contenttwo').removeClass('showprocess');
-            $('#contenttwo div').removeClass('showprocess');
-        }
-        if (steps == 3) {
-            $('#stepstitle').html("3. Submit And Wait");
-            $('#stepsdesc').html("First things first, we need to know what project you would like to start with us.");
-            $('#boxthree').addClass('activestep');
-            $('#contentthree').removeClass('showprocess');
-        }
+function differentSteps() {
+    if (steps == 1) {
+        $('#stepstitle').html("1. Choose Your Project");
+        $('#stepsdesc').html("First things first, we need to know what project you would like to start with.<br><br> From logos to flyers to web design, we can do anything branding!");
+        $('#boxone').addClass('activestep');
+        $('#contentone').removeClass('showprocess').fadeIn();
+        $('#contentone div').removeClass('showprocess');
     }
+    if (steps == 2) {
+        $('#stepstitle').empty();
+        $('#stepstitle').html("2. Take Our Survey");
+        $('#stepsdesc').html("Always have communication problems with freelancers?<br><br> Fill out our user friendly survey which takes less than 10 minutes!");
+        $('#boxtwo').addClass('activestep');
+        $('#contenttwo').removeClass('showprocess');
+        $('#contenttwo div').removeClass('showprocess');
+    }
+    if (steps == 3) {
+        $('#stepstitle').html("3. Submit And Wait");
+        $('#stepsdesc').html("That's it! We have all the information we need to start your sweet project.<br><br> Give us 24 hrs and we'll reach out to set up a free consultation meeting with you!");
+        $('#boxthree').addClass('activestep');
+        $('#contentthree').removeClass('showprocess');
+    }
+}
 
-    differentSteps();
+differentSteps();
 
-    $('#prev, #next, #boxone, #boxtwo, #boxthree, #rightarrow, #leftarrow').on('click', function() {
-        $('#vbcontent div').addClass('showprocess');
-        $('#diffsteps .circles').removeClass('activestep');
-        if ($(this).attr("id") == "prev" || $(this).attr("id") == "leftarrow") {
-            steps--;
-            if (steps == 0) {
-                steps = 3;
-            }
-        }
-        if ($(this).attr("id") == "next" || $(this).attr("id") == "rightarrow") {
-            steps++;
-            if (steps == 4) {
-                steps = 1;
-            }
-        }
-        if ($(this).attr("id") == "boxone") {
-            steps = 1;
-        }
-        if ($(this).attr("id") == "boxtwo") {
-            steps = 2;
-        }
-        if ($(this).attr("id") == "boxthree") {
+$('#prev, #next, #boxone, #boxtwo, #boxthree, #rightarrow, #leftarrow').on('click', function() {
+    $('#vbcontent div').addClass('showprocess');
+    $('#diffsteps .circles').removeClass('activestep');
+    if ($(this).attr("id") == "prev" || $(this).attr("id") == "leftarrow") {
+        steps--;
+        if (steps == 0) {
             steps = 3;
         }
-        differentSteps();
-    });
+    }
+    if ($(this).attr("id") == "next" || $(this).attr("id") == "rightarrow") {
+        steps++;
+        if (steps == 4) {
+            steps = 1;
+        }
+    }
+    if ($(this).attr("id") == "boxone") {
+        steps = 1;
+    }
+    if ($(this).attr("id") == "boxtwo") {
+        steps = 2;
+    }
+    if ($(this).attr("id") == "boxthree") {
+        steps = 3;
+    }
+    differentSteps();
+});
+
+// faq
+$('#sec4 button').on('click', function() {
+    $('#sec4 button').removeClass('changeborder');
+    if ($(this).attr("aria-expanded") == "false") {
+        $(this).addClass('changeborder');
+    }
+});
+
 
 
 $('#chooseproject').hide();
