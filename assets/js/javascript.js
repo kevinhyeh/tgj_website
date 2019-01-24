e9af0b7d30a979488686ed3a2a6359b6495d5520
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro.local>
2019-01-23 16:29:59 -0800

    swapped out logo. added more questions to survey(color picker, about the project, about the company). deleted unecessary images

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,77 +9,81 @@ $('#intro-button').html(button);
 // header scroll function
 if (window.location.href == "file:///Users/kevinyeh/Desktop/Desktop/the_graphic_jar/tgj_website/index.html") {
     $(document).on('scroll', function() {
-        var span = $('<span>').html("graphic");
-        var h1 = "the" + span + "jar";
+        var graphic = $('<span>').html("graphic");
+        var h1 = `the ${graphic} jar`;
         if ($(document).scrollTop()) {
             $('nav').addClass('scroll-change');
             $('#intro-button').empty();
             $('#nav-button').html(button);
-            $('.tgj-logo').attr('src', 'assets/svg/tgj_logo(scroll).svg');
+            $('.tgj-logo').attr('src', 'assets/svg/tgj_new_logo.svg');
+            $('h1').css('display', 'none');
+            $('.nav-links').find('a').css('color', '#000');
         } else {
             $('nav').removeClass('scroll-change');
             $('#nav-button').empty();
             $('#intro-button').html(button);
-            $('.tgj-logo').attr('src', 'assets/svg/tgj_logo.svg');
+            $('.tgj-logo').attr('src', 'assets/svg/tgj_new_logo(white).svg');
+            $('h1').css('display', 'inline-block');
+            $('.nav-links').find('a').css('color', '#fff');
         }
     });
 }
 
 // process slideshow
-var steps = 1;
+// var steps = 1;
 
-function differentSteps() {
-    if (steps == 1) {
-        $('#stepstitle').html("1. Choose Your Project");
-        $('#stepsdesc').html("First things first, we need to know what project you would like to start with.<br><br> From logos to flyers to web design, we can do anything branding!");
-        $('#boxone').addClass('activestep');
-        $('#contentone').removeClass('showprocess');
-        $('#contentone div').removeClass('showprocess');
-    }
-    if (steps == 2) {
-        $('#stepstitle').empty();
-        $('#stepstitle').html("2. Take Our Survey");
-        $('#stepsdesc').html("Always have communication problems with freelancers?<br><br> Fill out our user friendly survey which takes less than 10 minutes!");
-        $('#boxtwo').removeClass('activestep');
-        $('#contenttwo').removeClass('showprocess');
-        $('#contenttwo div').removeClass('showprocess');
-    }
-    if (steps == 3) {
-        $('#stepstitle').html("3. Submit And Wait");
-        $('#stepsdesc').html("That's it! We have all the information we need to start your sweet project.<br><br> Give us 24 hrs and we'll reach out to set up a free consultation meeting with you!");
-        $('#boxthree').removeClass('activestep');
-        $('#contentthree').removeClass('showprocess');
-    }
-}
+// function differentSteps() {
+//     if (steps == 1) {
+//         $('#stepstitle').html("1. Choose Your Project");
+//         $('#stepsdesc').html("First things first, we need to know what project you would like to start with.<br><br> From logos to flyers to web design, we can do anything branding!");
+//         $('#boxone').addClass('activestep');
+//         $('#contentone').removeClass('showprocess');
+//         $('#contentone div').removeClass('showprocess');
+//     }
+//     if (steps == 2) {
+//         $('#stepstitle').empty();
+//         $('#stepstitle').html("2. Take Our Survey");
+//         $('#stepsdesc').html("Always have communication problems with freelancers?<br><br> Fill out our user friendly survey which takes less than 10 minutes!");
+//         $('#boxtwo').removeClass('activestep');
+//         $('#contenttwo').removeClass('showprocess');
+//         $('#contenttwo div').removeClass('showprocess');
+//     }
+//     if (steps == 3) {
+//         $('#stepstitle').html("3. Submit And Wait");
+//         $('#stepsdesc').html("That's it! We have all the information we need to start your sweet project.<br><br> Give us 24 hrs and we'll reach out to set up a free consultation meeting with you!");
+//         $('#boxthree').removeClass('activestep');
+//         $('#contentthree').removeClass('showprocess');
+//     }
+// }
 
-differentSteps();
+// differentSteps();
 
-$('#prev, #next, #boxone, #boxtwo, #boxthree, #rightarrow, #leftarrow').on('click', function() {
-    $('#vbcontent div').addClass('showprocess');
-    $('#diffsteps .circles').addClass('activestep');
-    if ($(this).attr("id") == "prev" || $(this).attr("id") == "leftarrow") {
-        steps--;
-        if (steps == 0) {
-            steps = 3;
-        }
-    }
-    if ($(this).attr("id") == "next" || $(this).attr("id") == "rightarrow") {
-        steps++;
-        if (steps == 4) {
-            steps = 1;
-        }
-    }
-    if ($(this).attr("id") == "boxone") {
-        steps = 1;
-    }
-    if ($(this).attr("id") == "boxtwo") {
-        steps = 2;
-    }
-    if ($(this).attr("id") == "boxthree") {
-        steps = 3;
-    }
-    differentSteps();
-});
+// $('#prev, #next, #boxone, #boxtwo, #boxthree, #rightarrow, #leftarrow').on('click', function() {
+//     $('#vbcontent div').addClass('showprocess');
+//     $('#diffsteps .circles').addClass('activestep');
+//     if ($(this).attr("id") == "prev" || $(this).attr("id") == "leftarrow") {
+//         steps--;
+//         if (steps == 0) {
+//             steps = 3;
+//         }
+//     }
+//     if ($(this).attr("id") == "next" || $(this).attr("id") == "rightarrow") {
+//         steps++;
+//         if (steps == 4) {
+//             steps = 1;
+//         }
+//     }
+//     if ($(this).attr("id") == "boxone") {
+//         steps = 1;
+//     }
+//     if ($(this).attr("id") == "boxtwo") {
+//         steps = 2;
+//     }
+//     if ($(this).attr("id") == "boxthree") {
+//         steps = 3;
+//     }
+//     differentSteps();
+// });
 
 // faq
 $('.faq button').on('click', function() {
@@ -100,21 +104,21 @@ $('.faq2 button').on('click', function() {
     }
 });
 
-// form
-$('#projectform').addClass('hide');
-$('#thankyou').addClass('hide');
+// // form
+// $('#projectform').addClass('hide');
+// $('#thankyou').addClass('hide');
 
-$('.projectsBut').on('click', function() {
-    $('#projects').addClass('hide');
-    $('#chosenproject').html($(this).html());
-    $('#projectform').removeClass('hide');
-});
+// $('.projectsBut').on('click', function() {
+//     $('#projects').addClass('hide');
+//     $('#chosenproject').html($(this).html());
+//     $('#projectform').removeClass('hide');
+// });
 
-$('#backBut').on('click', function() {
-    event.preventDefault();
-    $('#projectform').addClass('hide');
-    $('#projects').removeClass('hide');
-});
+// $('#backBut').on('click', function() {
+//     event.preventDefault();
+//     $('#projectform').addClass('hide');
+//     $('#projects').removeClass('hide');
+// });
 
 // Initialize Firebase
 var config = {
--- survey/client/src/App.js
+++ survey/client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { _loadProjects } from './services/fetchCalls';
+// import { _loadProjects } from './services/fetchCalls';
+import projectsJSON from './projects.json';
+import { SketchPicker } from 'react-color';
 
 class App extends Component {
 
@@ -9,16 +11,34 @@ class App extends Component {
       projects: [],
       steps: 0,
       finalPrice: 0,
-      projectsChosen: [],
-      companyName: '',
-      complexityChosen: []
+      projectsChosen: [{
+        project: null,
+        price: null
+      }],
+      complexityChosen: [{
+        complexity: null,
+        price: null
+      }],
+      primColor: "",
+      secColor: "",
+      projBudget: "",
+      projTimeline: "",
+      projDesc: "",
+      businessName: "",
+      businessWeb: "",
+      businessDesc: "",
+      businessSlogan: "",
+      businessAud: ""
     };
   };
 
   componentDidMount() {
-    return _loadProjects().then(resultingJSON => this.setState({ projects: resultingJSON }))
+    // return _loadProjects().then(resultingJSON => this.setState({ projects: resultingJSON }))
+    this.setState({ projects: projectsJSON })
+
   }
 
+  // global functions
   totalPrice = () => { 
     let finalPrice = 0
     let projArrPrice = this.state.projectsChosen.map(a => a.price);
@@ -44,7 +64,16 @@ class App extends Component {
     this.setState({ steps: this.state.steps - 1});
   }
 
+  // project functions
   chooseFirstProject = (project, price) => {
+    if (this.state.projectsChosen[0].project != project) {
+      this.setState({ complexityChosen: [
+        {
+          complexity: null,
+          price: null
+        }
+      ]})
+    }
     // this.state.projectsChosen.push(project);
     this.setState({ projectsChosen: [
       {
@@ -58,17 +87,6 @@ class App extends Component {
     this.setState({ steps: 1 });
   }
 
-  inputName = (e) => {
-    this.setState({ companyName: e.target.value });
-  }
-
-  inputNameBut = () => {
-    if (this.state.companyName == "") {
-      this.setState({ companyName: "No Input"});
-    }
-    this.nextStep();
-  }
-
   chooseComplexity = (complex, price) => {
     this.setState({ complexityChosen: [
       {
@@ -79,15 +97,74 @@ class App extends Component {
     setTimeout(function() {
       this.totalPrice();
     }.bind(this), 1)
+    this.nextStep();
   } 
 
+  choosePrimColor = (color) => {
+    this.setState({ primColor: color.hex });
+  }
+
+  chooseSecColor = (color) => {
+    this.setState({ secColor: color.hex })
+  }
+
+  cancelColor = () => {
+    this.setState({ primColor: "" });
+    this.setState({ secColor: "" });
+  } 
+
+  noInput = (value, state) => {
+    switch (value == "") {
+      case state == "primColor":
+        this.setState({ primColor: "No Colors"});
+        break;
+      case state == "projDesc":
+        this.setState({ projDesc: "No Input" });
+        break;
+      case state == "businessName":
+        this.setState({ businessName: "No Input" }); 
+    }
+    this.nextStep();
+  }
+
+  // company functions
+  inputText = (e, state) => {
+    switch (state) {
+      case "projBudget":
+        this.setState({ projBudget: e.target.value });
+        break;
+      case "projTimeline":
+        this.setState({ projTimeline: e.target.value });
+        break;
+      case "projDesc":
+        this.setState({ projDesc: e.target.value });
+        break;
+      case "businessName":
+        this.setState({ businessName: e.target.value });
+        break;
+      case "businessWeb":
+        this.setState({ businessWeb: e.target.value });
+        break;
+      case "businessDesc":
+        this.setState({ businessDesc: e.target.value });
+        break;
+      case "businessSlogan":
+        this.setState({ businessSlogan: e.target.value });
+        break;
+      case "businessAud":
+        this.setState({ businessAud: e.target.value });
+    }
+  }
+
   render() {
-    const projChosen = this.state.projectsChosen.length === 1;
-    const nameChosen = this.state.companyName.length > 0;
-    const compChosen = this.state.complexityChosen.length === 1;
+    const projChosen = this.state.projectsChosen[0].project != null;
+    const compChosen = this.state.complexityChosen[0].complexity != null;
+    const colorChosen = this.state.primColor.length > 0;
+    const descChosen = this.state.projDesc.length > 0 || this.state.projBudget.length > 0 || this.state.projTimeline.length > 0;
+    const nameChosen = this.state.businessName.length > 0;
 
     const projectsList = this.state.projects.map(project => {
-      return <button onClick={() => this.chooseFirstProject(project.project, project.price)} key={project.id}>{project.project}</button>
+      return <button onClick={() => this.chooseFirstProject(project.project, project.price)} key={project._id}>{project.project}</button>
     });
 
     return (
@@ -96,24 +173,72 @@ class App extends Component {
           Estimated Starting Price: ${this.state.finalPrice}
         </div>
         <div>
-          { this.state.steps == 0 ? (
+          { this.state.steps === 0 ? (
             <div>
               <p>What would you like to start with us?</p>
               {projectsList}
             </div>
-            ) : this.state.steps == 1 ? (
+            ) : this.state.steps === 1 ? (
             <div>
-              <p>What's your company?</p>
-              <input type="text" placeholder="Your Company Name" onChange={e => this.inputName(e)} value={this.state.companyName} />
-              <button onClick={() => this.inputNameBut()}>&#62;</button>
+              <p>How complex would you like your {this.state.projectsChosen[0].project.toLowerCase()} be?</p>
+              { this.state.projectsChosen[0].project === "Custom Logo" ? (
+                <div>
+                  <button onClick={() => this.chooseComplexity("Simple", 0)}>Simple +$0</button>
+                  <button onClick={() => this.chooseComplexity("Complicated", 50)}>Complicated +$50</button>
+                </div>
+                ) : this.state.projectsChosen[0].project === "Business Card" ? (
+                <div>
+                  <button onClick={() => this.chooseComplexity("Single Side", 0)}>Single Side +$0</button>
+                  <button onClick={() => this.chooseComplexity("Double Side", 100)}>Double Side +$100</button>
+                </div>
+                ) : this.state.projectsChosen[0].project === "Website" ? (
+                <div>
+                  <button onClick={() => this.chooseComplexity("Single Page", 0)}>Single Page +$0</button>
+                  <button onClick={() => this.chooseComplexity("Multiple Pages", 200)}>Multiple Pages +$200</button>
+                </div>
+                ) : null
+              }
             </div>
-            ) : this.state.steps == 2 ? (
-              <div>
-                <p>How complex would you like your {this.state.projectsChosen[0].project.toLowerCase()} be?</p>
-                <button onClick={() => this.chooseComplexity("Simple", 0)}>Simple +$0</button>
-                <button onClick={() => this.chooseComplexity("Moderate", 20)}>Moderate +$20</button>
-                <button onClick={() => this.chooseComplexity("Complicated", 50)}>Complicated +$50</button>
-              </div>
+            ) : this.state.steps === 2 ? (
+            <div>
+              <p>What's your two main colors? It's fine if you have only one color</p>
+              <p>Primary</p>
+              <SketchPicker color={this.state.primColor} onChange={this.choosePrimColor} />
+              { this.state.primColor.split('')[0] == "#" ? (
+                <div>
+                  <p>Secondary</p>
+                  <SketchPicker color={this.state.secColor} onChange={this.chooseSecColor} />
+                  <button onClick={() => this.cancelColor()}>Cancel Color</button>
+                </div>
+                ): null
+              }
+              <button onClick={() => this.noInput(this.state.primColor, "primColor")}>&#62;</button>
+            </div>
+            ) : this.state.steps === 3 ? (
+            <div>
+              <p>What is your budget for this {this.state.projectsChosen[0].project.toLowerCase()}?</p>
+              <input type="text" placeholder="Budget" onChange={e => this.inputText(e, "projBudget")} value={this.state.projBudget} />
+              <p>Do you have a timeline for this project?</p>
+              <input type="text" placeholder="Budget" onChange={e => this.inputText(e, "projTimeline")} value={this.state.projTimeline} />
+              <p>Is there anything specific about your project that you'd like to add?</p>
+              <textarea rows="10" style={{ width: "40%" }} placeholder="Comment here" onChange={e => this.inputText(e, "projDesc")} value={this.state.projDesc} />
+              <button onClick={() => this.noInput(this.state.projDesc, "projDesc")}>&#62;</button>
+            </div>
+            ) : this.state.steps === 4 ? (
+            <div>
+              <p>Tell Us About Your Business</p>
+              <p>What's the name of your business?</p>
+              <input type="text" placeholder="Name of business" onChange={e => this.inputText(e, "businessName")} value={this.state.businessName} />
+              <p>Do you have a website for your business?</p>
+              <input type="text" placeholder="Website" onChange={e => this.inputText(e, "businessWeb")} value={this.state.businessWeb} />
+              <p>What is your business?</p>
+              <textarea rows="10" style={{ width: "40%" }} placeholder="Comment here" onChange={e => this.inputText(e, "businessDesc")} value={this.state.businessDesc} />
+              <p>Do you have a motto/slogan?</p>
+              <textarea rows="4" style={{ width: "40%" }} placeholder="Comment here" onChange={e => this.inputText(e, "businessSlogan")} value={this.state.businessSlogan} />
+              <p>Who is your target audience?</p>
+              <input type="text" placeholder="Audience" onChange={e => this.inputText(e, "businessAud")} value={this.state.businessAud} />
+              <button onClick={() => this.noInput(this.state.businessName, "businessName")}>&#62;</button>
+            </div>
             ) : null
           }
         </div>
@@ -126,17 +251,28 @@ class App extends Component {
             </div>
             ) : null
           }
-          { nameChosen ? (
+          { compChosen ? (
             <div>
-              <p onClick={() => this.goToStep(1)}>Company Name</p>
-              <p>{this.state.companyName}</p>
+              <p onClick={() => this.goToStep(1)}>Complexity</p>
+              <p>{this.state.complexityChosen[0].complexity} +${this.state.complexityChosen[0].price}</p>
             </div>
             ) : null
           }
-          { compChosen ? (
+          { colorChosen ? (
             <div>
-              <p onClick={() => this.goToStep(2)}>Complexity</p>
-              <p>{this.state.complexityChosen[0].complexity} +${this.state.complexityChosen[0].price}</p>
+              <p onClick={() => this.goToStep(2)}>Color</p>
+              <p>{this.state.primColor}</p>
+              <p>{this.state.secColor}</p>
+            </div>
+            ) : null
+          }
+          { descChosen ?
+            <p onClick={() => this.goToStep(3)}>Project Info</p> 
+            : null
+          }
+          { nameChosen ? (
+            <div>
+              <p onClick={() => this.goToStep(4)}>Business Information</p>
             </div>
             ) : null
           }
@@ -144,7 +280,6 @@ class App extends Component {
         {/*next and prev button*/}
         <div>
           <button onClick={() => this.prevStep()}>&#60;</button>
-          <button onClick={() => this.nextStep()}>&#62;</button>
         </div>
       </div>
     );
