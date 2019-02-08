a7ab81b1dcb0a43d194dc5c2e263b7d0ac91eb32
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro.local>
2019-02-08 15:14:09 -0800

    made a different repository for the survey

--- survey/db/seeds.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO projects (project, price, description)
-VALUES ('Custom Logo', 400, 'Custom logo for your company'), ('Single Side Business Card', 150, 'Have all your information on a beautiful one sided business card'), ('Dual Side Business Card', 250, 'Increase the design by adding a second side to your card'); 
